8e6e1faf9596db64afd9060e9a2fc3d5d480e20f
debymaurel <97979751+debymaurel@users.noreply.github.com>
2022-01-18 23:44:27 +0000

    Add files via upload

---
+++ Bonjour.java
@@ -0,0 +1,10 @@
+    /* Bonjour.java : ce programme affiche un message de bienvenue */
+
+  class Bonjour {                                // déclaration class
+
+      public static void main (String arg[]) {   // methode main()
+          System.out.println ("Bonjour");        // instruction : permet d'afficher le contenu du message
+
+      }
+
+  }
---
+++ Bonjour3.java
@@ -0,0 +1,8 @@
+
+public class Bonjour3 {
+	
+	public Static void main (String arg[]) {
+		System.out.println ("Bonjour" + arg[objis]);
+		}
+
+}
